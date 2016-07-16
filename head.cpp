5a8a4b94cfce505b0e8ec432b163ea4cd10e764d
azure <fyppyyxx@126.com>
2016-07-16 21:04:48 +0800

    often used head files and macros

---
+++ head.cpp
@@ -0,0 +1,22 @@
+#include<cstdio>
+#include<iostream>
+#include<iomanip>
+#include<cstdlib>
+#include<cmath>
+#include<cstring>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<map>
+#include<set>
+#include<algorithm>
+#include<utility>
+#include<functional>
+#define For(i, n) for(int i = 0; i < (n); ++i)
+using namespace std;
+
+int main()
+{
+    return 0;
+}
