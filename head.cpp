fdcc870945f98218609744cb7596600eaea326c9
azure <fyppyyxx@126.com>
2016-08-04 22:21:27 +0800

    ?

--- head.cpp
+++ head.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<cstdio>
 #include<iostream>
 #include<iomanip>
@@ -14,9 +15,21 @@
 #include<utility>
 #include<functional>
 #define For(i, n) for(int i = 0; i < (n); ++i)
+#define Ri(x) scanf("%d",&(x))
+#define R2i(x,y) scanf("%d %d",&(x),&(y))
+#define Pi(x) printf("%d",(x))
+#define Piln(x) printf("%d\n",(x))
 using namespace std;
 
 int main()
 {
+    while(0){
+	For(i,n);
+	int x,y;
+	Ri(x);
+	R2i(x,y);
+	Pi(x);
+	Piln(x);
+    }
     return 0;
 }
